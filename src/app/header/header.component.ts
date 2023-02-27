b035b1388156787afe534accc55f058f58bd275c
EmilBoye <emilboye1419@gmail.com>
2023-02-27 21:24:46 +0100

    Hoho

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -11,21 +11,29 @@ import { HttpService } from '../service/httpservice.service';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  createUserForm: any = new FormGroup({});
-  users: User[] = [];
-  confirmPass: any;
-  @Input() nyBruger = {userId: 0, roleId: 0, role:RoleType.User, userName: "", passwordHash: ""}
+  // createUserForm: any = new FormGroup({});
+  // users: User[] = [];
+  // // confirmPass: any;
+  // @Input() nyBruger = {roleId: 0, role:RoleType.User, userName: "", passwordHash: ""}
   userChecked: boolean = true;
   showCreateModal = false;
   showLoginModal = false;
-  userId: number[] = [];
+  userId: number;
 
+
+  user:any = {
+    id:0,
+    roleId:0,
+    role:RoleType.User,
+    userName:'',
+    passwordHash:''
+  }
   constructor(private service:HttpService, private formBuilder:FormBuilder) { }
 
 
   loginForm = new FormGroup({
-    userId: new FormControl(0),
-    roleId: new FormControl(0),
+    userId: new FormControl(),
+    roleId: new FormControl(),
     userName: new FormControl('', [Validators.required]),
     passwordHash: new FormControl('', [Validators.required]),
     roleType: new FormControl(RoleType.User),
@@ -33,63 +41,60 @@ export class HeaderComponent implements OnInit {
   });
 
   ngOnInit(): void {
-    this.createUserForm = this.formBuilder.group({
-        userName: new FormControl('', [Validators.required]),
-        role: new FormControl(RoleType.User),
-        passwordHash: new FormControl('', [Validators.required]),
-        confirmPass: new FormControl('')
-    });
-    this.service.getAllUsers().subscribe(u=>this.users = u);
+    // this.createUserForm = this.formBuilder.group({
+    //   roleId: new FormControl(''),
+    //   userName:new FormControl('', [Validators.required]),
+    //   role:this.formBuilder.control(RoleType.User),
+    //   passwordHash:new FormControl('', [Validators.required]),
+    // });
 
 
   }
   onCreate():void{
     this.showCreateModal = true;
-    console.warn("createUserForm",this.createUserForm.value);
+    console.log("User", this.user.value);
+
+    // console.warn("createUserForm",this.createUserForm.value);
   }
   onLogin():void{
     this.showLoginModal = true;
     console.warn("loginForm", this.loginForm.value);
 
+    this.service.getAllUsers().subscribe(u=>{
+      console.log(u);
+
+    });
   }
   onSubmitCreate():void{
-    this.confirmPass = this.createUserForm.get('confirmPass');
-      if(this.createUserForm.value.passwordHash?.length >= 8 && this.createUserForm.value.userName){
-        alert("Brugeren er oprettet");
+    // this.confirmPass = this.createUserForm.get('confirmPass');
 
-        this.service.postUser(this.createUserForm.value).subscribe(user => console.log(user));
-        if(this.createUserForm.value.userName.length < 1){
-          alert("Du skal skrive dit brugernavn korrekt. Minimum 3 karakter");
-        }
-      }
-      else{
-        if(this.createUserForm.value.passwordHash?.length < 8 ){
-          alert("Adgangskoden skal være minimum 8 karakter langt.");
-        }
-      }
+    if(this.user.valid){
+      this.service.postUser(this.user).subscribe(res=>{
+        this.user = res;
+        console.log(this.user);
+
+      })
+    }
+      // if(this.createUserForm.value.passwordHash?.length >= 8 && this.createUserForm.value.userName){
+      //   alert("Brugeren er oprettet");
+
+      //   //this.service.postUser(this.createUserForm.value).subscribe(user => console.log(user));
+      //   if(this.createUserForm.value.userName.length < 1){
+      //     alert("Du skal skrive dit brugernavn korrekt. Minimum 3 karakter");
+      //   }
+      // }
+      // else{
+      //   if(this.createUserForm.value.passwordHash?.length < 8 ){
+      //     alert("Adgangskoden skal være minimum 8 karakter langt.");
+      //   }
+      // }
   }
-  onSubmitLogin(): void {
-    var userFilter = this.users.filter(u => u.userName == this.loginForm.value.userName && u.passwordHash == this.loginForm.value.passwordHash);
-
-    if(userFilter.length == 0){
-      alert("Forkert brugernavn eller adgangskode");
-      this.userChecked = false;
-    }
-
-    else if(userFilter.length == 1){
-      this.userChecked = true;
-      this.service.postUser(this.nyBruger).subscribe();
-      this.loginForm.reset();
-      window.localStorage.setItem('User', userFilter[0].id.toString());
-      window.location.reload();
-      alert("Du er nu logget ind!");
-      this.userChecked = false;
-    }
+  onSubmitLogin(id:number): void {
     //this.userObject.userName = this.loginForm.value.userName;
-    this.service.getUserById(this.nyBruger.userId).subscribe(response=>{
+    this.service.getUserById(id).subscribe(response=>{
       if(response){
         console.log(response);
-        this.users = response;
+
       }
     })
   }
