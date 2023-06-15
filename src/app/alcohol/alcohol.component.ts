9c921be2cad3a66cd2ed0a8597908047e91317f6
EmilBoye <emilboye1419@gmail.com>
2023-06-15 12:58:04 +0200

    Kommentarfeltet er færdigt!
    Projektet kan nu kaldes for færdigt. Næsten

--- src/app/alcohol/alcohol.component.ts
+++ src/app/alcohol/alcohol.component.ts
@@ -17,54 +17,57 @@ import { Rating } from '../models/Rating-model';
 export class AlcoholComponent implements OnInit {
   drinks: Alcohol[] = [];
   searchValue: string = '';
-  newComment:string = '';
+  newComment: string = '';
   showSearch: boolean = false;
 
   ratings: Rating[];
-  ratingComment:Rating;
-  user: HeaderComponent[] = [];
+  ratingComment: Rating;
+  users: User[] = [];
+
+  userName: User;
   userId: number;
   selectedDrinkId: number;
 
-  constructor(private alcoholService:HttpService, private router:Router, private authService:AuthService) { }
-
+  constructor(private alcoholService: HttpService, private router: Router, private authService: AuthService) {}
 
   ngOnInit(): void {
+    this.ratingComment = new Rating();
     this.userId = JSON.parse(localStorage.getItem('User') || 'null');
     console.log(this.userId);
 
-    this.alcoholService.getAllDrinks().subscribe(a => {
+    this.alcoholService.getAllDrinks().subscribe((a) => {
       this.drinks = a;
-      console.log("Alkohol",a);
-      this.alcoholService.getAllComments().subscribe(x=>{
+      console.log("Alkohol", a);
+      this.alcoholService.getAllComments().subscribe((x) => {
         this.ratings = x;
-        console.log("comment",x);
-
+        console.log("comment", x);
+        this.alcoholService.getAllUsers().subscribe((x) =>{
+          this.users = x;
+          console.log("users",x);
+        });
       });
     });
-    this.ratingComment = new Rating();
     alert("Denne side er kun for personer over 18 år.");
   }
 
-
-  createDrink():void {
+  createDrink(): void {
     //Hvis brugeren er logget ind får man tilladelse her til at gå videre
-    if(this.authService.isLoggedIn()) {
-     this.router.navigate(['alkohol/tilføj']);
+    if (this.authService.isLoggedIn()) {
+      this.router.navigate(['alkohol/tilføj']);
     }
     //Hvis brugeren ikke er logget ind får man en alert og bliver navigeret til login.
-    else{
+    else {
       alert("Du skal være logget ind!");
       this.router.navigate(['login']);
     }
   }
 
-  editDrink(id:any): void {
-    this.router.navigate(['alkohol/opdater/',id])
+  editDrink(id: any): void {
+    this.router.navigate(['alkohol/opdater/', id]);
   }
 
-  deleteDrink(drinkId:any): void {
-    this.alcoholService.deleteDrink(drinkId).subscribe(a=> {
+  deleteDrink(drinkId: any): void {
+    this.alcoholService.deleteDrink(drinkId).subscribe((a) => {
       console.log(drinkId);
     });
   }
@@ -72,24 +75,32 @@ export class AlcoholComponent implements OnInit {
   showSearchValue(): void {
     this.showSearch = true;
   }
+
   searchDrinks(): void {
-    this.alcoholService.getAllDrinks().subscribe(drinks => {
-      this.drinks = drinks.filter(drink =>
+    this.alcoholService.getAllDrinks().subscribe((drinks) => {
+      this.drinks = drinks.filter((drink) =>
         drink.ingredients.toLowerCase().includes(this.searchValue.toLowerCase())
       );
     });
   }
-  postComment(drinkID:number):void{
+
+  postComment(drinkID: number): void {
     this.ratingComment.drinkId = drinkID;
-    this.alcoholService.postComment(this.ratingComment).subscribe(x=>{
-      const rating = new Rating();
-      rating.drinkId = drinkID;
+    this.ratingComment.userId = this.userId;
+    // Assign the username of the current user to the ratingComment
+    this.ratingComment.user = this.userName;
+
+    this.alcoholService.postComment(this.ratingComment).subscribe((x) => {
       console.log(x);
+
+      // Refresh the comments after posting a new comment
+      this.alcoholService.getAllComments().subscribe((x) => {
+        this.ratings = x;
+      });
     });
   }
 
-  selectDrink(drinkId:number):void{
+  selectDrink(drinkId: number): void {
     this.selectedDrinkId = drinkId;
   }
-
 }
